366d1d83db07b490c51c1e3a1c5b8c3f935b2f80
Victoria Walker <vmsmith2014@gmail.com>
2021-10-26 10:00:53 -0400

    socket.io working; still working on final touches of styling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io');
 const port = process.env.PORT || 3000;
 
 // static assets
@@ -13,6 +14,15 @@ app.use(require('./routes/albums'));
 app.use(require('./routes/message_board'));
 app.use(require('./routes/chat'));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
+});
+
+let io = socket(server);
+
+// listen for client messages
+io.on('connection', (socket) => {
+    socket.on('postMessage', (msgClient) => { // listening for incoming chat messages
+        io.emit('updateMessage', msgClient) // broadcast back out to all of the clients
+    });
 });
